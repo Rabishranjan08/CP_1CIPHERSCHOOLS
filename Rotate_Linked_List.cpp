4820fa6920fb994e5b7b9e8f79f0097b4bbb4597
Rabish Ranjan <30564326+Rabishranjan08@users.noreply.github.com>
2020-08-30 09:56:46 +0530

    Add files via upload

---
+++ Rotate_Linked_List.cpp
@@ -0,0 +1,22 @@
+Node* rotate(Node* head, int k)
+{
+    if(k==0)
+        return head;
+    Node* current=head;
+    int count=1;
+    while(count<k && current!=NULL){
+        current=current->next;
+        count++;
+    }
+    if(current==NULL){
+        return head;
+    }
+    Node* temp=current;
+    while(current->next){
+        current=current->next;
+    }
+    current->next=head;
+    head=temp->next;
+    temp->next=NULL;
+    return head;
+}
